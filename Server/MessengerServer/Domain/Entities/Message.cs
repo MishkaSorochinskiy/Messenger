7514dbca53ccf4ee3ee7434bc4b78069d9953d6c
mishash20161506 <mishash20161506@gmail.com>
2020-01-22 11:04:22 +0200

    Added entities and their configurations with dbcontext added migration and generated database

---
+++ Server/MessengerServer/Domain/Entities/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Message
+    {
+        public int Id { get; private set; }
+
+        public string Content { get; set; }
+
+        public DateTime? TimeCreated { get; set; }
+
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+    }
+}
