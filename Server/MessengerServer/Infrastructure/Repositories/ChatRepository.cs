bfe961856df6907b1e57e4499d81c9cff9ec3c41
mishash20161506 <mishash20161506@gmail.com>
2020-02-06 16:43:04 +0200

    added repositories

---
+++ Server/MessengerServer/Infrastructure/Repositories/ChatRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using Domain.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Repositories
+{
+   public class ChatRepository: Repository<Chat>, IChatRepository
+    {
+        public ChatRepository(MessengerContext db):base(db)
+        {
+
+        }
+    }
+}
