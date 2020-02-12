bda7d0563a2d68532de9f477c4a42b8d290fb5a7
mishash20161506 <mishash20161506@gmail.com>
2020-02-12 12:48:41 +0200

    swagger with UI added

--- Server/MessengerServer/MessengerAPI/Startup.cs
+++ Server/MessengerServer/MessengerAPI/Startup.cs
@@ -19,6 +19,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace MessengerAPI
 {
@@ -73,6 +74,12 @@ namespace MessengerAPI
 
             services.AddAutoMapper(typeof(MappingProfile));
 
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Chatter Messenger", Version = "v1" });
+            });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -84,6 +91,13 @@ namespace MessengerAPI
             }
 
             app.UseHttpsRedirection();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseRouting();
 
