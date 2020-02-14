ba149beae787eeab796a1575dcf190acb7431e76
mishash20161506 <mishash20161506@gmail.com>
2020-02-14 14:35:18 +0200

    added all needed exceptions

---
+++ Server/MessengerServer/Domain/Exceptions/BlockedUserExceptions/BlockedUserAlreadyExist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Exceptions.BlockedUserExceptions
+{
+    public class BlockedUserAlreadyExist:BaseException
+    {
+        public BlockedUserAlreadyExist(string message,int statusCode):base(message,statusCode)
+        {
+
+        }
+    }
+}
---
+++ Server/MessengerServer/Domain/Exceptions/ChatExceptions/ChatAlreadyExistException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Exceptions.ChatExceptions
+{
+    public class ChatAlreadyExistException:BaseException
+    {
+        public ChatAlreadyExistException(string message,int statusCode):base(message,statusCode)
+        {
+
+        }
+    }
+}
