b78be5660bd5986d5d06463eeaa2d4300de97887
mishash20161506 <mishash20161506@gmail.com>
2020-02-21 17:17:02 +0200

    last tests done

--- Server/MessengerServer/ApplicationTests/ChatServiceTests/CreateChatTest.cs
+++ Server/MessengerServer/ApplicationTests/ChatServiceTests/CreateChatTest.cs
@@ -1,11 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
+
+using Application.Models.ChatDto.Requests;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using Domain;
+using Domain.Entities;
+using Domain.Exceptions.ChatExceptions;
+using Domain.Exceptions.UserExceptions;
+using Infrastructure.Services;
+using Moq;
+using Xunit;
 
 namespace ApplicationTests.ChatServiceTests
 {
     public class CreateChatTest
     {
+        [Fact]
+        public async void CreateChat_UserNotExist_ThrowsException()
+        {
+            //arrange
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
 
+            var mockAuth = fixture.Freeze<Mock<IAuthService>>();
+            mockAuth.Setup(a => a.FindByNameUserAsync(It.IsAny<string>()))
+                .ReturnsAsync(default(User));
+
+            var chatService = fixture.Create<ChatService>();
+
+            //assert
+            await Assert.ThrowsAsync<UserNotExistException>
+                (async () => await chatService.CreateChatAsync(new AddChatRequest()));
+        }
+
+        [Fact]
+        public async void CreateChat_ChatExist_ThrowsException()
+        {
+            //arrange
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+            var mockUnit = fixture.Freeze<Mock<IUnitOfWork>>();
+            mockUnit.Setup(a => a.ChatRepository.ChatExistAsync(It.IsAny<int>(),It.IsAny<int>()))
+                .ReturnsAsync(false);
+
+            var chatService = fixture.Create<ChatService>();
+
+            //assert
+            await Assert.ThrowsAsync<ChatAlreadyExistException>
+                (async () => await chatService.CreateChatAsync(new AddChatRequest()));
+        }
+
+        [Fact]
+        public async void CreateChat_Creat_InvokesOnce()
+        {
+            //arrange
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+            var mockUnit = fixture.Freeze<Mock<IUnitOfWork>>();
+            mockUnit.Setup(a => a.ChatRepository.ChatExistAsync(It.IsAny<int>(), It.IsAny<int>()))
+                .ReturnsAsync(true);
+
+            var chatService = fixture.Create<ChatService>();
+
+            //act
+            await chatService.CreateChatAsync(new AddChatRequest());
+
+            //assert
+            mockUnit.Verify(u => u.ChatRepository.CreateAsync(It.IsAny<Chat>()), Times.Once);
+        }
     }
 }
