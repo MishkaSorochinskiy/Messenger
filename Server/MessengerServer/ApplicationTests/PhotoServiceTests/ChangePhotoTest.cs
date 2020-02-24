645a36a8f6dca19d71d774dbb0b16caba5231370
mishash20161506 <mishash20161506@gmail.com>
2020-02-24 11:23:58 +0200

    removed comments

--- Server/MessengerServer/ApplicationTests/PhotoServiceTests/ChangePhotoTest.cs
+++ Server/MessengerServer/ApplicationTests/PhotoServiceTests/ChangePhotoTest.cs
@@ -1,17 +1,10 @@
 ﻿using Application.Models.PhotoDto;
 using AutoFixture;
 using AutoFixture.AutoMoq;
-using Domain;
 using Domain.Entities;
 using Domain.Exceptions.UserExceptions;
 using Infrastructure.Services;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Xunit;
 
 namespace ApplicationTests.PhotoServiceTests
@@ -34,37 +27,5 @@ namespace ApplicationTests.PhotoServiceTests
             await Assert.ThrowsAsync<UserNotExistException>
                 (async () => await photoService.ChangePhotoAsync(new AddPhotoDto()));
         }
-
-        //[Fact]
-        //public async void ChangePhoto_GetPhoto_InvokesOnce()
-        //{
-        //    //arrange
-        //    var fixture = new Fixture().Customize(new AutoMoqCustomization());
-
-        //    var mockUnit = fixture.Freeze<Mock<IUnitOfWork>>();
-
-        //    var mockFile = new Mock<IFormFile>();
-        //    mockFile.Setup(f => f.FileName)
-        //        .Returns("Test.Extension");
-
-        //    var mockConfigSection = fixture.Freeze<Mock<IConfigurationSection>>();
-        //    mockConfigSection.Setup(confSec => confSec.Get(typeof(string[])))
-        //        .Returns(new string[] { });
-
-        //    var mockConfig= fixture.Freeze<Mock<IConfiguration>>();
-        //    mockConfig.Setup(c => c.GetSection(It.IsAny<string>()))
-        //        .Returns(mockConfigSection.Object);
-
-        //    var photoService = fixture.Create<PhotoService>();
-
-        //    //act
-        //    await photoService.ChangePhotoAsync(new AddPhotoDto() 
-        //    {
-        //        UploadedFile=mockFile.Object
-        //    });
-
-        //    //assert
-        //    mockUnit.Verify(u => u.PhotoRepository.GetPhotoByUserAsync(It.IsAny<int>()), Times.Once);
-        //}
     }
 }
